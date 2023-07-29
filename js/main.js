96a5582922911cb06c0722b5b59d56512749573e
Dmitry Shentyakov <the_scorpion1408@inbox.ru>
2023-07-29 13:25:51 +0300

    Добавляет слайдер эффектов

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ import {data} from './data.js';
 import {renderPictures} from './pictures.js';
 import './user-modal.js';
 import './form.js';
-import './range-slider.js'
+import './range-slider.js';
 renderPictures(data);
