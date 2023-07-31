2c85fa1aba2ccb8029c445e7737bddb6e1639f0d
Dmitry Shentyakov <the_scorpion1408@inbox.ru>
2023-07-31 16:02:55 +0300

    Подготавливает проект к защите

--- js/load.js
+++ js/load.js
@@ -1,12 +1,15 @@
 import {getData} from './api.js';
 import {showAlertError} from './util.js';
 import {renderPictures} from './pictures.js';
+import {initializeFilters} from './picture-filter.js';
+
 
 let data = null;
 
 try {
   data = await getData();
   renderPictures(data);
+  initializeFilters();
 } catch {
   showAlertError('Ваши данные очень важны для нас. В настоящий момент они не загружены. Попробуйте перезагрузить страницу!');
 }
